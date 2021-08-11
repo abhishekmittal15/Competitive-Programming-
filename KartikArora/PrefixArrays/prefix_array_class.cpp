ca95ff7395db714fedf9ce7a582abc385a1a6433
abhishekmittal15 <55945158+abhishekmittal15@users.noreply.github.com>
2021-08-11 15:33:36 +0530

    closes #1; Now you can create custom functions and pass them to the class

--- KartikArora/PrefixArrays/prefix_array_class.cpp
+++ KartikArora/PrefixArrays/prefix_array_class.cpp
@@ -1,31 +1,42 @@
 #include<iostream>
 #include<vector>
+#include<functional>
 using namespace std;
 #define int long long int 
-
-int f(int a,int b){
-    return a + b;
-}
-int g(int a,int b){
-    return a - b;
-}
 class PrefixArray{
     private:
+        vector<int> input;
         vector<int> p;
         int sz = 0;
     public:
         PrefixArray(vector<int>& a){
             sz = a.size();
-            p.push_back(0);
-            for (int i = 0;i<sz;i++)
-                p.push_back(f(p[i],a[i]));
+            for (int i = 0; i < sz;i++){
+                input.push_back(a[i]);
+            }
         }
-        int query(int a,int b,int index=1){
+        void construct(int base,function<int(int, int)> f)
+        {
+            p.push_back(base);
+            for (int i = 0; i < sz;i++){
+                p[i + 1] = f(p[i], input[i]);
+            }
+        }
+        int query(function<int(int,int)> g,int a,int b,int index=1){
             if(index==0)
                 a++, b++;
             return g(p[b],p[a-1]);
         }
 };
+
+int f(int a, int b)
+{
+    return a + b;
+}
+int g(int a, int b)
+{
+    return a - b;
+}
 
 signed main(){
     int n;
@@ -34,12 +45,13 @@ signed main(){
     for (int i = 0; i < n;i++)
         cin >> a[i];
     PrefixArray pref(a);
+    pref.construct(0,&f);
     int q;
     cin >> q;
     while(q--){
         int a, b;
         cin >> a >> b;
-        cout << pref.query(a, b) << endl;
+        cout << pref.query(&g,a, b) << endl;
     }
     return 0;
 }
